a37838d6dfc742102e52c58b531b38af57c780c9
codex-06 <80632985+codex-06@users.noreply.github.com>
2023-02-02 03:03:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var roll1 = Math.floor((Math.random() * 6) + 1);
-var img1  = "images/dice" + roll1 + ".png"
+var img1  = "dice" + roll1 + ".png"
 var roll2 = Math.floor((Math.random() * 6) + 1);
-var img2  = "images/dice" + roll2 + ".png"
+var img2  = "dice" + roll2 + ".png"
 document.querySelector(".dice1").setAttribute("src",img1);
 document.querySelector(".dice2").setAttribute("src",img2);
 if (img1>img2){
