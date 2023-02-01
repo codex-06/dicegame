310c97e0fa36fe1f145db2ea749fad2f78b9d422
codex-06 <80632985+codex-06@users.noreply.github.com>
2023-02-02 03:03:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var roll1 = Math.floor((Math.random() * 6) + 1);
+var img1  = "images/dice" + roll1 + ".png"
+var roll2 = Math.floor((Math.random() * 6) + 1);
+var img2  = "images/dice" + roll2 + ".png"
+document.querySelector(".dice1").setAttribute("src",img1);
+document.querySelector(".dice2").setAttribute("src",img2);
+if (img1>img2){
+    document.querySelector(".title").textContent ="player 1 wins"
+}
+else if ( img1 == img2){
+    document.querySelector(".title").textContent ="Draw"
+}
+else{
+    document.querySelector(".title").textContent ="player 2 wins"
+}
